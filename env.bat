619c75ba0ba6258bb9da43108feeb3a8f1d52aab
Roman <charoman@gmail.com>
2018-03-13 10:31:22 +0200

    changes

--- env.bat
+++ env.bat
@@ -1,5 +1,9 @@
+docker-machine start
+
 SET DOCKER_TLS_VERIFY=1
 SET DOCKER_HOST=tcp://192.168.99.100:2376
 SET DOCKER_CERT_PATH=C:\Users\rcha\.docker\machine\machines\default
 SET DOCKER_MACHINE_NAME=default
 SET COMPOSE_CONVERT_WINDOWS_PATHS=true
+
+docker-compose up
